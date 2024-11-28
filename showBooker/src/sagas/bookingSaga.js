2fb00272f926845385480c53ec2f5a66adee92b9
Anees Hasainar <aneeshassainar786@gmail.com>
2024-11-28 19:46:58 +0530

    last commit

--- showBooker/src/sagas/bookingSaga.js
+++ showBooker/src/sagas/bookingSaga.js
@@ -1,12 +1,21 @@
 import { takeEvery, call, put,delay } from 'redux-saga/effects';
 import { bookMovie,setLoading } from '../Redux/bookingSlice';
+import { bookMovieService } from '../services/services';
+
 
 
 function* bookMoviesSaga(action) {
     try {
+        console.log(action.payload);
         yield put(setLoading(true));
-        yield delay(3000);
-        yield put(bookMovie({ ...action.payload}));
+        const result=yield bookMovieService(action.payload)
+        if(result.responseIndicator=="success"){
+
+           yield put(bookMovie({ ...action.payload}));
+           alert("booking successfull")
+        }else{
+            alert("Booking Failed")
+        }
       } catch (error) {
         console.error("Error in booking saga:", error);
       } finally {
